4ddeea07db0b1ba1165cf3a792a4140b89d39d91
Tracy Quick <tlquick999@gmail.com>
2023-02-09 09:58:33 +1100

    add lombok configuration

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.time.LocalDate;
+
 import java.time.Period;
 
 import jakarta.persistence.Entity;
@@ -10,69 +11,50 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.AccessLevel;
 
-@Entity //this class describes objects
-@Table //this class represents a table in the db
+@Entity // this class describes objects
+@Table // this class represents a table in the db
+@Data // lombok - combines @Getter & @Setter - generates setters and getters
+@NoArgsConstructor // lombok - generates the no arg constructor
+@ToString(includeFieldNames = true) // lombok - generate toString() with field names included
 public class Student {
-@Id //this is the primary key for the table - below defines the sequence for auto increment
-@SequenceGenerator(name="student_sequence", sequenceName= "student_sequence", allocationSize=1)
-@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="student_sequence") 
- private Long id;
- private String name;
- private String email;
- private LocalDate dob;
- @Transient //calculated value - not stored in DB
- private Integer age;
- 
- 
-public Student() {
-	super();
-}
-public Student(Long id, String name, String email, LocalDate dob) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.email = email;
-	this.dob = dob;
-}
-public Student(String name, String email, LocalDate dob) {
-	super();
-	this.name = name;
-	this.email = email;
-	this.dob = dob;
-}
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public LocalDate getDob() {
-	return dob;
-}
-public void setDob(LocalDate dob) {
-	this.dob = dob;
-}
-public Integer getAge() {
-	return Period.between(this.dob , LocalDate.now()).getYears(); //calculate the years between dob & now
-}
+	@Id // this is the primary key for the table - below defines the sequence for auto
+		// increment
+	@SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+	private Long id;
+	private String name;
+	private String email;
+	private LocalDate dob;
+	@Transient // calculated value - not stored in DB
+	@Getter(AccessLevel.NONE) // lombok - do not create getter for this field
+	@Setter(AccessLevel.NONE) // lombok - do not create setter for this field
+	@ToString.Exclude // lombok - do not add this field to the toString()
+	private Integer age;
 
-@Override
-public String toString() {
-	return "Student [id=" + id + ", name=" + name + ", email=" + email + ", dob=" + dob + ", age=" + age + "]";
-}
+	public Student(Long id, String name, String email, LocalDate dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+	}
 
- 
+	public Student(String name, String email, LocalDate dob) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+	}
+
+	public Integer getAge() {
+		return Period.between(this.dob, LocalDate.now()).getYears(); // calculate the years between dob & now
+	}
+
 }
